cd6727150cbb9499cff1763beffe9c6f4ae2bf10
Christian Tielking <tielking5@gmail.com>
2021-10-04 14:04:13 -0400

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,7 @@
+const amount = 9;
+
+if (amount < 10) {
+    console.log('small number');
+} else {
+    console.log('large number');
+};
---
+++ 4-names.js
@@ -0,0 +1,6 @@
+const secret = 'thedebtthatallmenpay'
+const shaker = 'Shaker'
+const steve = 'Steve'
+module.exports = { shaker, steve }
+
+// console.log(module);
